b5b2565da89bc9c6181747c48e7b91d6d5b39a76
M00staff <Mostafa@nete.com>
2017-03-16 15:53:24 -0400

    correct data is rendering, set up alert messages next

--- frontend/App.js
+++ frontend/App.js
@@ -8,7 +8,7 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      inputValue: '',
+      inputValue: 'bart@fullcontact.com',
       dataToRender: null,
     };
     this.grabInput = this.grabInput.bind(this);
@@ -37,11 +37,19 @@ class App extends Component {
     if (this.state.dataToRender !== null) {
       return (
         console.log(this.state.dataToRender),
-          this.state.dataToRender.socialProfiles.map((data, index) =>
-            <ul key={index} className={styles.persons}>
-              <li>{data.bio}</li>
-            </ul>,
-        )
+          <div>
+
+            <img src={this.state.dataToRender.photos[0].url} alt="guy" height="42" width="42" />
+            <div>{this.state.dataToRender.contactInfo.fullName}</div>
+            <div>{this.state.dataToRender.demographics.locationGeneral}</div>
+
+            {this.state.dataToRender.socialProfiles.map(data =>
+              <ul key={data.id} className={styles.persons}>
+                <li key={data.id}>{data.url}</li>
+              </ul>,
+          )}
+
+          </div>
       );
     }
     return null;
@@ -53,7 +61,7 @@ class App extends Component {
         <form>
           <input type="text" value={this.state.inputValue} onChange={this.grabInput} />
         </form>
-        <button type="submit" value="Submit" onClick={this.renderContact} />
+        <input type="submit" value="Submit" onClick={this.renderContact} />
         <div>{ this.renderItems() }</div>
       </div>
     );
